982b6b1c74231b6379c9aba1f8177212b3d0c23f
Austin Lawrence <Ablarry91@gmail.com>
2014-11-27 11:57:22 -0500

    Got everything to plot properly.  Several nodes are being plotted in obstacles, wrongly, however.

--- run.py
+++ run.py
@@ -18,20 +18,20 @@ Turn in your well-commented code, as well as the graphical output for one exampl
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def createTargets(size):
 	import random
-	targets = np.zeros(4)
+	targets = np.zeros([2,2])
 	for i in range(2):
-		targets[i] = random.uniform(size[0],size[1])
-		targets[i+2] = random.uniform(size[2],size[3])
+		targets[i,0] = random.uniform(size[0],size[1])
+		targets[i,1] = random.uniform(size[2],size[3])
 	# print 'my targets are','\n',targets
 	return targets
 
-def createObstacles(size, targets):
+def createObstacles(size, targets, quantity):
 	import random
-	quantity = 10 #number of obstacles to create
-	radii = np.array([5,10]) #the range of possible radii for the obstacles
+	radii = np.array([10,20]) #the range of possible radii for the obstacles
 	obstacles = np.zeros([quantity, 3]) #Nx3 matrix, columns are X,Y, and radius
 
 	#creates a random set of obstacles of random size
@@ -50,19 +50,32 @@ def createObstacles(size, targets):
 
 def checkIntersection(targets, nodeX, nodeY, nodeRadius):
 	#checks to see if the prospective nodes intersect with the start or finish targets
-	radiusStart = np.sqrt(np.power(targets[0]-nodeX,2)+np.power(targets[1]-nodeY,2))
-	radiusGoal = np.sqrt(np.power(targets[2]-nodeX,2)+np.power(targets[3]-nodeY,2))
-	if radiusStart < nodeRadius or radiusGoal < nodeRadius:
-		print 'got an intersection'
-		return False
-	else:
-		return True
+	for i in range(len(targets)):
+		distance = np.sqrt(np.power(targets[i,0]-nodeX,2)+np.power(targets[i,1]-nodeY,2))
+		if distance < nodeRadius:
+			# print 'got an intersection at', nodeX, nodeY, nodeRadius
+			return False
+	return True
 
-def createNodes(size, obstacles):
-	pass
-
-def createNodes(size, obstacles):
-	pass
+def createNodes(size, obstacles, quantity):
+	#creates a random set of nodes
+	import random
+	nodes = np.zeros([quantity, 2]) #columns are X and Y
+	for i in range(quantity):
+		check = False
+		while check == False:
+			#try to create a node that does not overlap the obstacles
+			nodeX = random.uniform(size[0],size[1])
+			nodeY = random.uniform(size[2],size[3])
+			#checks for interception on each individual obstacle
+			for j in range(len(obstacles)):
+				targets = np.matrix([nodeX, nodeY])
+				check = checkIntersection(targets, obstacles[j,0], obstacles[j,1], obstacles[j,2])
+				if check:
+					nodes[i,0] = nodeX
+					nodes[i,1] = nodeY
+					break
+	return nodes
 
 def createEdges(nodes, obstacles, targets):
 	pass
@@ -72,8 +85,38 @@ def aStar():
 
 size = np.array([0,100,0,100]) #xMin, xMax, yMin, yMax
 targets = createTargets(size) #xStart, yStart, xGoal, yGoal
-obstacles = createObstacles(size, targets)
-nodes = createNodes(size, obstacles)
+obstacles = createObstacles(size, targets, 10) #xNode, yNode, radius
+nodes = createNodes(size, obstacles, 50)
 edges = createEdges(nodes, obstacles, targets)
 aStar()
+
+# plt.scatter(nodes[:,0], nodes[:,1], s=1, alpha=0.5)
+# # plt.scatter(targets[:,0], targets[:,1], s=500, c='red',alpha=0.5)
+# plt.scatter(50, 50, s=10000, c='red',alpha=0.5)
+# plt.scatter(obstacles[:,0], obstacles[:,1], s=obstacles[2], c='green',alpha=0.5)
+# plt.Circle((50,50),10,color='r')
+
+# plt.show()
+
+print obstacles,'\n',nodes
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+for i in range(len(obstacles)):
+	circ = plt.Circle((obstacles[i,0],obstacles[i,1]), radius = obstacles[i,2],alpha=0.8, color='blue')
+	ax.add_patch(circ)
+for i in range(len(targets)):
+	circ = plt.Circle((targets[i,0],targets[i,1]), radius = 3, color='red')
+	ax.add_patch(circ)
+for i in range(len(nodes)):
+	circ = plt.Circle((nodes[i,0],nodes[i,1]), radius = 1, color='black')
+	ax.add_patch(circ)
+plt.axis(size, aspect=1)	
+plt.show()
+
+
+
+
+
 
